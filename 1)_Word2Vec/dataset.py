0558cb81abbf266703d1bc6f7799581d7dce78c2
NirutG <59528991+NirutG@users.noreply.github.com>
2023-05-02 23:24:06 +0700

    Upload dataset.py

---
+++ 1)_Word2Vec/dataset.py
@@ -0,0 +1,61 @@
+table_tennis = """
+Table tennis, also known as ping-pong and whiff-whaff, is a racket sport derived from tennis but distinguished by its playing surface being atop a stationary table, rather than the court on which players stand. Either individually or in teams of two, players take alternating turns returning a light, hollow ball over the table's net onto the opposing half of the court using small rackets until they fail to do so, which results in a point for the opponent. Play is fast, requiring quick reaction and constant attention, and is characterized by an emphasis on spin relative to other ball sports, which can heavily affect the ball's trajectory.
+
+Owed to its small minimum playing area, its ability to be played indoors in all climates, and relative accessibility of equipment, table tennis is enjoyed worldwide not just as a competitive sport, but as a common recreational pastime among players of all levels and ages.
+
+Table tennis has been an Olympic sport since 1988,[3] with event categories in both men's and women's singles, and men's and women's teams since replacing doubles in 2008.
+
+Table tennis is governed by the International Table Tennis Federation (ITTF), founded in 1926, and specifies the official rules in the ITTF handbook.[4] ITTF currently includes 226 member associations worldwide.[5]
+
+The sport originated in Victorian England, where it was played among the upper-class as an after-dinner parlour game.[1][2] It has been suggested that makeshift versions of the game were developed by British military officers in India around the 1860s or 1870s, who brought it back with them.[6] A row of books stood up along the center of the table as a net, two more books served as rackets and were used to continuously hit a golf-ball.[7][8]
+
+The name "ping-pong" was in wide use before British manufacturer J. Jaques & Son Ltd trademarked it in 1901. The name "ping-pong" then came to describe the game played using the rather expensive Jaques's equipment, with other manufacturers calling it table tennis. A similar situation arose in the United States, where Jaques sold the rights to the "ping-pong" name to Parker Brothers. Parker Brothers then enforced its trademark for the term in the 1920s, making the various associations change their names to "table tennis" instead of the more common, but trademarked, term.[9]
+
+The next major innovation was by James W. Gibb, a British table tennis enthusiast, who discovered novelty celluloid balls on a trip to the US in 1901 and found them ideal for the game. This was followed by E.C. Goode who, in 1901, invented the modern version of the racket by fixing a sheet of pimpled, or stippled, rubber to the wooden blade. Table tennis was growing in popularity by 1901 to the extent that tournaments were being organized, books were being written on the subject,[7] and an unofficial world championship was held in 1902. In those early days, the scoring system was the same as in lawn tennis.[10]
+
+Although both a "Table Tennis Association" and a "Ping Pong Association" existed by 1910,[10] a new Table Tennis Association was founded in 1921, and renamed the English Table Tennis Association in 1926.[11] The International Table Tennis Federation (ITTF) followed in 1926.[1][12] London hosted the first official World Championships in 1926. In 1933, the United States Table Tennis Association, now called USA Table Tennis, was formed.[1][13]
+
+In the 1930s, Edgar Snow commented in Red Star Over China that the Communist forces in the Chinese Civil War had a "passion for the English game of table tennis" which he found "bizarre".[14] On the other hand, the popularity of the sport waned in the 1930s Soviet Union, partly because of the promotion of team and military sports, and partly because of a theory that the game had adverse health effects.[15]
+
+In the 1950s, paddles that used a rubber sheet combined with an underlying sponge layer changed the game dramatically,[1] introducing greater spin and speed.[16] These were introduced to Britain by sports goods manufacturer S.W. Hancock Ltd. The use of speed glue beginning in the mid-1980s increased the spin and speed even further, resulting in changes to the equipment to "slow the game down". Table tennis was introduced as an Olympic sport at the Olympics in 1988.[17]
+"""
+c = '''
+C (pronounced /ˈsiː/ – like the letter c)[6] is a general-purpose computer programming language. It was created in the 1970s by Dennis Ritchie, and remains very widely used and influential. By design, C's features cleanly reflect the capabilities of the targeted CPUs. It has found lasting use in operating systems, device drivers, protocol stacks, though decreasingly[7] for application software. C is commonly used on computer architectures that range from the largest supercomputers to the smallest microcontrollers and embedded systems.
+
+A successor to the programming language B, C was originally developed at Bell Labs by Ritchie between 1972 and 1973 to construct utilities running on Unix. It was applied to re-implementing the kernel of the Unix operating system.[8] During the 1980s, C gradually gained popularity. It has become one of the most widely used programming languages,[9][10] with C compilers available for practically all modern computer architectures and operating systems. C has been standardized by ANSI since 1989 (ANSI C) and by the International Organization for Standardization (ISO).
+
+C is an imperative procedural language, supporting structured programming, lexical variable scope and recursion, with a static type system. It was designed to be compiled to provide low-level access to memory and language constructs that map efficiently to machine instructions, all with minimal runtime support. Despite its low-level capabilities, the language was designed to encourage cross-platform programming. A standards-compliant C program written with portability in mind can be compiled for a wide variety of computer platforms and operating systems with few changes to its source code.[11]
+
+Since 2000, C has consistently ranked among the top two languages in the TIOBE index, a measure of the popularity of programming languages.[12]
+
+C is an imperative, procedural language in the ALGOL tradition. It has a static type system. In C, all executable code is contained within subroutines (also called "functions", though not in the sense of functional programming). Function parameters are passed by value, although arrays are passed as pointers, i.e. the address of the first item in the array. Pass-by-reference is simulated in C by explicitly passing pointers to the thing being referenced.
+
+C program source text is free-form code. The semicolon separates statements and curly braces are used for grouping blocks of statements.
+
+The C language also exhibits the following characteristics:
+
+The language has a small, fixed number of keywords, including a full set of control flow primitives: if/else, for, do/while, while, and switch. User-defined names are not distinguished from keywords by any kind of sigil.
+It has a large number of arithmetic, bitwise, and logic operators: +,+=,++,&,||, etc.
+More than one assignment may be performed in a single statement.
+Functions:
+Function return values can be ignored, when not needed.
+Function and data pointers permit ad hoc run-time polymorphism.
+Functions may not be defined within the lexical scope of other functions.
+Variables may be defined within a function, with scope.
+A function may call itself, so recursion is supported.
+Data typing is static, but weakly enforced; all data has a type, but implicit conversions are possible.
+User-defined (typedef) and compound types are possible.
+Heterogeneous aggregate data types (struct) allow related data elements to be accessed and assigned as a unit.
+Union is a structure with overlapping members; only the last member stored is valid.
+Array indexing is a secondary notation, defined in terms of pointer arithmetic. Unlike structs, arrays are not first-class objects: they cannot be assigned or compared using single built-in operators. There is no "array" keyword in use or definition; instead, square brackets indicate arrays syntactically, for example month[11].
+Enumerated types are possible with the enum keyword. They are freely interconvertible with integers.
+Strings are not a distinct data type, but are conventionally implemented as null-terminated character arrays.
+Low-level access to computer memory is possible by converting machine addresses to pointers.
+Procedures (subroutines not returning values) are a special case of function, with an untyped return type void.
+Memory can be allocated to a program with calls to library routines.
+A preprocessor performs macro definition, source code file inclusion, and conditional compilation.
+There is a basic form of modularity: files can be compiled separately and linked together, with control over which functions and data objects are visible to other files via static and extern attributes.
+Complex functionality such as I/O, string manipulation, and mathematical functions are consistently delegated to library routines.
+The generated code after compilation has relatively straightforward needs on the underlying platform, which makes it suitable for creating operating systems and for use in embedded systems.
+While C does not include certain features found in other languages (such as object orientation and garbage collection), these can be implemented or emulated, often through the use of external libraries (e.g., the GLib Object System or the Boehm garbage collector).
+'''
